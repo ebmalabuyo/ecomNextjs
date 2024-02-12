34383b612794b576ee5ab7858ae9793ced41cd34
ebmalabuyo <elijahbmalabuyo@gmail.com>
2024-02-12 11:46:17 -0800

    fixed SortbyTypes

--- utils/index.ts
+++ utils/index.ts
@@ -29,18 +29,23 @@ const sortByProperty = (prop : string, order = 1) => {
     if (order !== -1) order = 1;
     if (prop === '') return (a: Product, b : Product) => {return 0};
     
+
     if (prop === 'High') {
         prop = "price"
         return (a: Product, b : Product) => {
-        if (a[prop] > b[prop]) return -1;
-        if (a[prop] < b[prop]) return 1;
+        const value1 = a[prop as keyof Product] as number
+        const value2 = b[prop as keyof Product ] as number
+        if (value1 > value2) return -1;
+        if (value1 < value2) return 1;
         return 0;
       };}
 
 
     return (a: Product, b : Product) => {
-      if (a[prop] > b[prop]) return 1;
-      if (a[prop] < b[prop]) return -1;
+      const value1 = a[prop as keyof Product] as number
+        const value2 = b[prop as keyof Product ] as number
+      if (value1 > value2) return 1;
+      if (value1 < value2) return -1;
       return 0;
     };
   };
