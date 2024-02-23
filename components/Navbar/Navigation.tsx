a6605610d9c61c25ebed7be37d96fa69d4976c39
ebmalabuyo <elijahbmalabuyo@gmail.com>
2024-02-22 23:07:45 -0800

    added mobile button for nav

--- components/Navbar/Navigation.tsx
+++ components/Navbar/Navigation.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import { FaShoppingBag } from "react-icons/fa";
 import Link from 'next/link';
 import { FaShoppingCart } from "react-icons/fa";
@@ -7,31 +7,38 @@ import { SiNextdotjs } from "react-icons/si";
 import { useSession, signOut } from 'next-auth/react';
 import { useStore } from '@/utils/store';
 import { IoPersonOutline } from "react-icons/io5";
+import { FiAlignJustify } from "react-icons/fi";
 
 const Navigation = () => {
     const {data: session} = useSession()
     const cartLength = useStore((state) => state.cart.length)
     // const {cart} = useStore()
+    const [toggleNav, setToggleNav] = useState(false);
 
   return (
     <>
     {/* <StoreHelper cart={cart}/> */}
-    <nav className='flex justify-between p-2 text-sm md:mr-10 md:ml-10 m-2 '>
-        <div className=' hidden md:flex w-full gap-3 items-center'>
-            <a href='https://www.ebmcodes.tech/' target='_blank'><h3 >Meet the Developer</h3></a>
+    <nav className='flex justify-between p-2 text-xs md:text-sm mr-0 ml-2 md:mr-10 md:ml-10 m-2 '>
+        <div className='flex w-full items-center'>
+            <div className='w-full md:hidden'>
+            <FiAlignJustify size={30}/>
+            </div>
+            <div className='hidden md:flex gap-3'>
+            <a href='https://www.ebmcodes.tech/' target='_blank'><h3>Meet the Developer</h3></a>
             <a href='https://github.com/ebmalabuyo/portfolioNext'><h3 >Source Code</h3></a>
+            </div>
         </div>
 
-            <div className=' w-full flex justify-start md:justify-center gap-2 relative'>
+            <div className=' w-full flex justify-center gap-2 relative '>
             
-            <Link href={"/"} className='text-center flex justify-center flex-col items-center'>
+            <Link href={"/"} className='  text-center flex justify-end md:justify-center flex-col items-center'>
             <SiNextdotjs size={40} />
             <h1>SHOPPING</h1>
             </Link>
             </div>
 
 
-        <div className=' flex w-full justify-end items-center gap-4 '>
+        <div className=' flex w-full justify-end items-center gap-1 md:gap-4 '>
             <h3>
                {!session ? 
                <Link href="/login" className='flex'>
