b41c02e42a509f946804d4369810b626db3a1993
ebmalabuyo <elijahbmalabuyo@gmail.com>
2024-04-16 22:21:36 -0700

    adding bg image hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,26 @@
 
 import Image from "next/image";
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <div className="w-full flex flex-col items-center ">
-      <div className="p-6 border w-full max-w-6xl ">
-        <div className='bg-[#c28ca5] text-white flex flex-col items-center justify-center text-xs w-full h-32'>
+      <div className="p-2 md:p-6 border w-full max-w-6xl ">
+            <div className='bg-[#c28ca5] text-white flex flex-col items-center justify-center text-xs w-full h-32'>
                 <h1 className='bg-[#c28ca5] font-bold text-lg' >Up to 50% off spring sales</h1>
                 <p className='bg-[#c28ca5] text-center'>Spring is right around the corner. Find the items you need to ring in the new season!</p>
                 <p className='bg-[#c28ca5] text-center'>(JK this is not a real ecom site just a practice site)</p>
             </div>
-            <div className="w-96 h-96 overflow-hidden relative">
-            <Image
-            src={"/19.png"}
-            alt="Image for Store"
-            fill
-            />
+            <div className="flex justify-center">
+              <div className="overflow-hidden relative flex justify-center w-96 h-96 bg-hero bg-no-repeat">
+              <div className="absolute bottom-0 flex flex-col items-center">
+                    <p>New Arrivals</p>
+                    <h1>Summer Utility Edit</h1>
+                  <Link href={"/shop"}>Shop Now</Link>
+                </div>
+              </div>
+                
             </div>
       </div>
       
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
+        "hero": "url('/public/19.png')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
